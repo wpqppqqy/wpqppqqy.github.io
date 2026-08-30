b9ff528bf9875b0956bcd3f1b0584d2bdb91d0a1
wpqppqqy <wpqppqqy@163.com>
2026-08-30 23:32:04 +0800

    更新文件

--- js/blog.js
+++ js/blog.js
@@ -11,6 +11,24 @@ async function fetchAllDiscussions() {
         }
     });
     allPosts = await res.json();
+    allPosts = await Promise.all(
+        allPosts.map(async (post) => {
+            const commentsUrl = `https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/discussions/${post.number}/comments?per_page=100&sort=created&direction=desc`;
+            const commentsRes = await fetch(commentsUrl, {
+                headers: {
+                    "Accept": "application/vnd.github.html+json"
+                }
+            });
+            innerComments = await commentsRes.json();
+            innerComments = innerComments.sort((a, b) =>
+                new Date(b.created_at) - new Date(a.created_at)
+            );
+            return {
+                ...post,
+                comments: innerComments
+            };
+        })
+    );
     allPosts = allPosts.sort((a, b) =>
         new Date(b.created_at) - new Date(a.created_at)
     );
@@ -35,11 +53,30 @@ function renderPostList() {
 
 function renderSinglePost(post) {
     const createDate = new Date(post.created_at).toLocaleString('zh-CN');
-    contentEl.innerHTML = `
-                <h2 class="post-title">${post.title}</h2>
-                <div class="post-meta">发布于 ${createDate}</div>
-                <div class="post-content">${post.body_html}</div>
+    let commentsHtml = `<div class="comments-section"><h3>评论区 (${post.comments.length}条)</h3>`;
+    post.comments.forEach(comment => {
+        const commentDate = new Date(comment.created_at).toLocaleString('zh-CN');
+        commentsHtml += `
+                <div class="comment-item">
+                    <div class="post-meta">
+                        <img class="author-avatar" src="${comment.user.avatar_url}" alt="${comment.user.login}">
+                        <span>${comment.user.login}</span>
+                        <span>${commentDate}</span>
+                    </div>
+                    <div class="post-content">${comment.body_html}</div>
+                </div>
             `;
+    })
+    commentsHtml += `</div>`;
+    contentEl.innerHTML = `
+        <h2 class="post-title">${post.title}</h2>
+        <div class="post-meta">
+        <span>${post.user.login}</span>
+        <span>发布于 ${createDate}</span>
+        </div>
+        <div class="post-content">${post.body_html}</div>
+        ${commentsHtml}
+    `;
     backBtn.style.display = 'block';
 }
 backBtn.addEventListener('click', renderPostList);
